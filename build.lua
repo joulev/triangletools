684ab02e99f78da53dcda130309e793c98e1aef0
Vu Van Dung <vuvandungchelsea@gmail.com>
2020-05-06 16:51:02 +0700

    Change TDS structure (thanks to @Skillmon)

--- build.lua
+++ build.lua
@@ -3,3 +3,11 @@
 module = "triangletools"
 installfiles = {"*.sty", "*.code.tex"}
 packtdszip = true
+tdslocations = {
+  "tex/latex/triangletools/source/trtmessages.code.tex",
+  "tex/latex/triangletools/source/trtlinetools.code.tex",
+  "tex/latex/triangletools/source/trtbarycentric.code.tex",
+  "tex/latex/triangletools/source/trtdistance.code.tex",
+  "tex/latex/triangletools/source/trtspecialpoints.code.tex",
+  "tex/latex/triangletools/source/trtfrontend.code.tex"
+}
